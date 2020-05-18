e37cff689ebd0a454a6e9216b7f96b8695441e3d
scodek <skhalequ03@gmail.com>
2020-05-18 10:55:02 +0200

    Updates

---
+++ precache-manifest.50ec4ca12ea1e725f1f766b0cc436865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7317192495669f45d66480bb97679b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad54f3d61d93c1fad5f",
+    "url": "/static/js/2.7583cedc.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.7583cedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03819f3137af93c973e5",
+    "url": "/static/js/main.8b995570.chunk.js"
+  },
+  {
+    "revision": "38c89c6b718639d0e385",
+    "url": "/static/js/runtime-main.6693240f.js"
+  }
+]);
